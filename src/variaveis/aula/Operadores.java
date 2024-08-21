d329394b086cffff153a55cb81b6ed08823e553c
Renato Lopes <renato.lopes1984@gmail.com>
2024-08-20 22:16:26 -0300

    Commit inicial trilha java

---
+++ src/variaveis/aula/Operadores.java
@@ -0,0 +1,60 @@
+package variaveis.aula;
+
+public class Operadores {
+
+	public static void main(String[] args) {
+		
+		boolean condicao1 = true;
+		boolean condicao2 = true;
+		
+		if(condicao1 && condicao2) {
+			System.out.println("As duas condições são verdadeiras");
+		}
+		
+		if(condicao1 || condicao2) {
+			System.out.println("Uma das condições é verdadeira.");
+		}
+		
+		
+		
+		
+		
+		
+		/*String nomeUm = "Renato";
+		String nomeDois = new String("Renato");
+		
+		System.out.println(nomeUm.equals(nomeDois)); //METODO EQUALS PARA COMPARAR CONTEÚDOS DE OBJETOS
+		
+		
+		
+		
+		
+		int numero1 = 1;
+		int numero2 = 2;
+		
+		boolean simNao = numero1 == numero2;
+		System.out.println("numeoUm é igual a numeroDois? " + simNao);
+
+		
+		simNao = numero1 != numero2;		
+		System.out.println("numeroUm é diferente do numeroDois? " + simNao);
+		
+		simNao = numero1 > numero2;
+		System.out.println("numeroUm é maior do que o numeroDois? " + simNao);
+
+
+		
+		
+		/*a = 6;
+		b = 6;
+		String resultado = a==b ? "Verdadeiro" : "Falso"; //TERNARIO
+		if(a==b)
+			resultado = "Verdadeiro";
+			else
+				resultado = "Falso";*/
+		
+		
+
+	}
+
+}
